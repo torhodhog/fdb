8ef33a629f00580a85cbb4b0d512676d60d27079
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-03-11 21:55:42 +0100

    ready to publish

---
+++ dist/collections/Orders.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Orders = void 0;
+var yourOwn = function (_a) {
+    var user = _a.req.user;
+    if ((user === null || user === void 0 ? void 0 : user.role) === "admin")
+        return true;
+    return {
+        user: user === null || user === void 0 ? void 0 : user.id,
+    };
+};
+exports.Orders = {
+    slug: "orders",
+    admin: {
+        useAsTitle: "Your Orders",
+        description: "En oversikt over dine bestillinger hos Fotballdraktbutikken",
+    },
+    access: {
+        read: yourOwn,
+        update: function (_a) {
+            var req = _a.req;
+            return req.user.role === "admin";
+        },
+        delete: function (_a) {
+            var req = _a.req;
+            return req.user.role === "admin";
+        },
+        create: function (_a) {
+            var req = _a.req;
+            return req.user.role === "admin";
+        },
+    },
+    fields: [
+        {
+            name: "_isPaid",
+            type: "checkbox",
+            access: {
+                read: function (_a) {
+                    var req = _a.req;
+                    return req.user.role === "admin";
+                },
+                create: function () { return false; },
+                update: function () { return false; },
+            },
+            admin: {
+                hidden: true,
+            },
+            required: true,
+        },
+        {
+            name: "user",
+            type: "relationship",
+            admin: {
+                hidden: true,
+            },
+            relationTo: "users",
+            required: true,
+        },
+        {
+            name: "products",
+            type: "relationship",
+            relationTo: "products",
+            required: true,
+            hasMany: true,
+        },
+    ],
+};
---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: "Produkter",
+        value: "ui_kits",
+        featured: [
+            {
+                name: "Våre Favoritter",
+                href: "#",
+                imageSrc: "/ui-kits/mixed.jpg",
+            },
+            {
+                name: "Nye Drakter",
+                href: "#",
+                imageSrc: "/ui-kits/blue.jpg",
+            },
+            {
+                name: "Salg",
+                href: "#",
+                imageSrc: "/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        label: "Kontakt",
+        value: "icons",
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/icons/picks.jpg",
+            },
+            {
+                name: "Nye Drakter",
+                href: "#",
+                imageSrc: "/icons/new.jpg",
+            },
+            {
+                name: "Salg Icons",
+                href: "#",
+                imageSrc: "/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
