6510ea05752ce88e1587d0d840be396df5d8a7d9
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-03-14 09:00:55 +0100

    Små endinger

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,15 @@
-"use client";
+'use client'
 
-import { PropsWithChildren, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "@/trpc/client";
-import { httpBatchLink } from "@trpc/client";
+import { PropsWithChildren, useState } from 'react'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { trpc } from '@/trpc/client'
+import { httpBatchLink } from '@trpc/client'
 
 const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
@@ -15,19 +18,23 @@ const Providers = ({ children }: PropsWithChildren) => {
           fetch(url, options) {
             return fetch(url, {
               ...options,
-              credentials: "include",
-            });
+              credentials: 'include',
+            })
           },
         }),
       ],
     })
-  );
+  )
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <trpc.Provider
+      client={trpcClient}
+      queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
     </trpc.Provider>
-  );
-};
+  )
+}
 
-export default Providers;
+export default Providers
--- src/middelware.ts
+++ src/middelware.ts
@@ -1,16 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
-import { getServerSideUser } from "./lib/payload-utils";
+import { getServerSideUser } from './lib/payload-utils'
 
-export async function middelware(req: NextRequest){
-   const {nextUrl, cookies} = req
-   const {user} = await getServerSideUser(cookies)
+export async function middleware(req: NextRequest) {
+  const { nextUrl, cookies } = req
+  const { user } = await getServerSideUser(cookies)
 
-   if( 
-      user && ['/sign-in', '/sign-up'].includes(nextUrl.pathname)){
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
+  if (
+    user &&
+    ['/sign-in', '/sign-up'].includes(nextUrl.pathname)
+  ) {
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/`
+    )
+  }
 
-   }
-
-   return NextResponse.next()
+  return NextResponse.next()
 }
