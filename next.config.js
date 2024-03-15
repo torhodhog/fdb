58c29f1f097c405c4147086ca9a48750228b86c4
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-03-15 15:18:44 +0100

    nok et forsøk på bildene

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      remotePatterns: [
-        
-         {
-            protocol: "http",
-            hostname: "localhost",
-         },
-         {
-            protocol: "https",
-            hostname: "fotball-d-b-production.up.railway.app",
-         },
-      ],
+     domains: ['fotball-d-b-production.up.railway.app'],
    },
-};
-
-module.exports = nextConfig;
+ };
+ 
+ module.exports = nextConfig;
+ 
