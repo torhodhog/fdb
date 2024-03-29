42b3bbe2385fcc94a6c4228063b77545d167e712
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-03-29 17:21:17 +0100

    Hero fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ const Hero: FC = () => {
    return (
       <div className="relative flex lg:flex-row flex-col-reverse">
          <div className="relative w-full lg:block hidden mt-6" style={{ height: '600px' }}>
-            <Image src="/HERO.png" alt="Hero Logo" layout="fill" objectFit="cover" />
+            <Image src="/herofredagen.png" alt="Hero Logo" layout="fill" objectFit="cover" />
          </div>
          <div className="relative w-full pl-0 pr-0 block lg:hidden" style={{ height: '100vh' }}>
-            <Image src="/mobilhero.png" alt="Hero Logo Mobile" layout="fill" objectFit="cover" objectPosition="top" />
+            <Image src="/langfredagen.png" alt="Hero Logo Mobile" layout="fill" objectFit="cover" objectPosition="top" />
             <Link href="/products">
                <div className="absolute left-1/2 transform -translate-x-1/2 z-10 bg-slate-50 rounded-md p-1" style={{ top: '90%' }}>
                   <span className="text-sm font-extrabold text-pink-700 hover:text-blue-500">Se hele kolleksjonen</span> <span aria-hidden="true">&rarr;</span>
