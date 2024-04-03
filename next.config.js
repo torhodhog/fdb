23f58be9804a2f096bd0cf0274c61e972d1786c3
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-04-03 10:15:17 +0200

    Back to images in next.config.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 const nextConfig = {
-   images: {
-      remotePatterns: [
-         'fotballdb.no/*',
-         'localhost/*',
-         // Legg til riktig URL for S3-bøtten din her
-         'forsoker-ny-botte.s3.amazonaws.com/*',
-      ],
-   },
+  images: {
+    domains: ['fotballdb.no', 'localhost', 'forsoker-ny-botte.s3.amazonaws.com'],
+  },
 };
 
 module.exports = nextConfig;
