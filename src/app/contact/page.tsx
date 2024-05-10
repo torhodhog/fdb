a7030e0684cb8ee407fe039b4547ad628d1566ab
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-05-10 15:00:16 +0200

    Kontakt virker.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,15 @@
 'use client'
-import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import React, { useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import emailjs from '@emailjs/browser';
+import { toast } from 'sonner';  // Importerer toast fra sonner, antar at biblioteket eksporterer en funksjon kalt toast
 
 export default function Contact() {
+  const form = useRef<HTMLFormElement | null>(null);
   const [formState, setFormState] = useState({
-    email: '',
-    phone: '',
-    first: '',
-    last: '',
+    user_email: '',
+    user_name: '',
     message: '',
   });
 
@@ -18,121 +19,105 @@ export default function Contact() {
       [e.target.name]: e.target.value,
     });
   };
-  
+
+  const resetForm = () => {
+    setFormState({
+      user_email: '',
+      user_name: '',
+      message: '',
+    });
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const message = {
-      name: `${formState.first} ${formState.last}`,
-      email: formState.email,
-      phone: formState.phone,
-      message: formState.message,
-    };
-
-    try {
-      const response = await fetch('/contact', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(message),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      console.log('Email sent');
-    } catch (error) {
-      console.error('Failed to send email', error);
+    if (form.current) {
+      emailjs
+        .sendForm('service_6lo1mcj', 'template_xu5g40h', form.current, 'TgOIRuM_0f-3pFshq')
+        .then(() => {
+          toast.success('Melding sendt uten problemer!');
+          resetForm();
+        }, (error) => {
+          toast.error('Failed to send email: ' + error.text);
+          console.error('Failed to send email', error.text);
+        });
+    } else {
+      console.error('Form reference is null');
+      toast.error('Form reference is not available');
     }
   };
+
   return (
     <>
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col
-      mb-28
-      mt-28 items-center justify-center w-full max-w-lg mx-auto p-12 bg-gray-800 shadow-xl rounded-lg space-y-4"
-    >
-      <h1 className="text-3xl font-bold text-white">Kom i kontakt med oss</h1>
+      <form
+        ref={form}
+        onSubmit={handleSubmit}
+        className="flex flex-col mb-28 mt-28 items-center justify-center w-full max-w-lg mx-auto p-12 bg-gray-800 shadow-xl rounded-lg space-y-4"
+      >
+        <h1 className="text-3xl font-bold text-white">Kom i kontakt med oss</h1>
 
-      <div className="flex flex-col w-full">
-        <label htmlFor="frm-email" className="text-gray-300">
-          Email
-        </label>
-        <input
-          id="frm-email"
-          type="email"
-          name="email"
-          className="p-2 rounded bg-gray-700 text-white border border-gray-600"
-          required
-          onChange={handleChange}
-        />
-      </div>
+        <div className="flex flex-col w-full">
+          <label htmlFor="frm-email" className="text-gray-300">
+            Email
+          </label>
+          <input
+            id="frm-email"
+            type="email"
+            name="user_email"
+            value={formState.user_email}
+            className="p-2 rounded bg-gray-700 text-white border border-gray-600"
+            required
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="flex w-full space-x-2">
-        <div className="flex flex-col w-1/2">
+        <div className="flex flex-col w-full">
           <label htmlFor="frm-first" className="text-gray-300">
-            Fornavn
+            Navn
           </label>
           <input
             id="frm-first"
             type="text"
-            name="first"
+            name="user_name"
+            value={formState.user_name}
             className="p-2 rounded bg-gray-700 text-white border border-gray-600"
             required
             onChange={handleChange}
           />
         </div>
 
-        <div className="flex flex-col w-1/2">
-          <label htmlFor="frm-last" className="text-gray-300">
-            Etternavn
+        <div className="flex flex-col w-full">
+          <label htmlFor="frm-message" className="text-gray-300">
+            Melding
           </label>
-          <input
-            id="frm-last"
-            type="text"
-            name="last"
+          <textarea
+            id="frm-message"
+            name="message"
+            value={formState.message}
             className="p-2 rounded bg-gray-700 text-white border border-gray-600"
-            required
             onChange={handleChange}
-          />
+          ></textarea>
         </div>
+
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300"
+        >
+          Send inn
+        </button>
+      </form>
+
+      <div className="flex justify-center items-center flex-col gap-y-3 mb-8">
+        <p>
+          <FontAwesomeIcon icon={faMapMarkerAlt} color="green" /> Brunestykket 2, 5039 Bergen
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faEnvelope} color="green" /> Mail: fdb@fotballdraktbutikken.com
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faPhone} color="green" /> Telefon: +47 979 39 973
+        </p>
       </div>
-
-      <div className="flex flex-col w-full">
-        <label htmlFor="frm-message" className="text-gray-300">
-          Melding
-        </label>
-        <textarea
-          id="frm-message"
-          name="message"
-          className="p-2 rounded bg-gray-700 text-white border border-gray-600"
-          onChange={handleChange}
-        ></textarea>
-      </div>
-
-      <button
-        type="submit"
-        className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300"
-      >
-        Submit
-      </button>
-    </form>
-  
-
-<div className="flex justify-center items-center flex-col gap-y-3 mb-8">
-  <p>
-    <FontAwesomeIcon icon={faMapMarkerAlt} color="green" /> Brunestykket 2, 5039 Bergen
-  </p>
-  <p>
-    <FontAwesomeIcon icon={faEnvelope} color="green" /> Mail: fdb@fotballdraktbutikken.com
-  </p>
-  <p>
-    <FontAwesomeIcon icon={faPhone} color="green" /> Telefon: +47 979 39 973
-  </p>
-</div>
-</>
-);
+    </>
+  );
 }
