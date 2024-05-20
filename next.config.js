684eb1b99807cda6e298fdb58b88a9faa012fc10
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-05-20 12:33:59 +0200

    updates next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   images: {
-    domains: ['fotballdb.no', 'localhost', 'forsoker-ny-botte.s3.amazonaws.com'],
+    domains: ['fotballdb.no', 'localhost', 'forsoker-ny-botte.s3.eu-north-1.amazonaws.com'],
   },
 };
 
