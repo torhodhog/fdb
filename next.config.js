8cd1077e4d1bacbd0785cad9ea8de1f5c219f6f5
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-05-28 22:13:42 +0200

    Prøver igjen

--- next.config.js
+++ next.config.js
@@ -10,7 +10,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'https://fotballdb.no',
     // Legg til andre miljøvariabler hvis nødvendig
-  }
+  },
+  reactStrictMode: true, // Legg til dette for å aktivere strict mode
+  swcMinify: true, // Legg til dette for å aktivere SWC minifier
 };
 
 module.exports = nextConfig;
