8f2bde625a8bc9d040db3ea645ccaa30f029eae5
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-09-03 21:25:11 +0200

    Smaller hero image

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 const HeroImage: FC = () => {
   return (
-    <div className="flex justify-center items-center w-full h-auto mt-8 bg-transparent"> {/* Sørg for at containeren er transparent */}
+    <div className="flex justify-center items-center w-full h-auto mt-8 bg-transparent"> 
       <Image
         src="https://forsoker-ny-botte.s3.eu-north-1.amazonaws.com/logo-5.png"
-        className="max-w-lg md:max-w-2xl lg:max-w-4xl"
+        className="max-w-lg md:max-w-2xl lg:max-w-xl"
         layout="responsive"
         alt="Hero"
         width={1920}
