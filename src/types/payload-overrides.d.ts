5c7f304a584730ffda7eef0385a8c1f22a422d7f
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-09-17 13:27:19 +0200

    Trying to fix duplicate

---
+++ src/types/payload-overrides.d.ts
@@ -0,0 +1,12 @@
+declare module 'payload' {
+   // Overstyr GeneratedTypes her
+   export interface GeneratedTypes {
+     collections: {
+       [slug: number | string | symbol]: Record<string, unknown>;
+     };
+     globals: {
+       [slug: number | string | symbol]: Record<string, unknown>;
+     };
+   }
+ }
+ 
