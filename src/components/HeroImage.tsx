6723bfdd946a9f7bc751ad1d78916b600766f4a5
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-10-07 16:23:44 +0200

    New Hero image

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -7,7 +7,7 @@ const HeroImage: FC = () => {
   return (
     <div className="flex justify-center items-center w-full h-auto  bg-transparent z-1"> 
       <Image
-        src="https://forsoker-ny-botte.s3.eu-north-1.amazonaws.com/Hero-Photoroom.png"
+        src="https://forsoker-ny-botte.s3.eu-north-1.amazonaws.com/bilde1.webp"
         className="w-full h-auto"
         layout="responsive"
         alt="Hero"
